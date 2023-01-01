482ccaa4e4f4ab4aa7e8dcc4ec0ac7d4297e86e7
Helmi Pradita <helmipraditaa@gmail.com>
2023-01-01 15:07:12 +0700

    finish API skill

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const UserRouter = require('../routes/user');
+const SkillRouter = require('../routes/skill');
 
 router.use('/auth', UserRouter);
+router.use('/skill', SkillRouter);
 
 module.exports = router;
